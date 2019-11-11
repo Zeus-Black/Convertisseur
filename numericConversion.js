c15faf855c8d81a2b311b4494493eb782b418f49
Zeus-Black <55084809+Zeus-Black@users.noreply.github.com>
2019-11-11 17:54:08 +0100

    Add files via upload

---
+++ numericConversion.js
@@ -0,0 +1,43 @@
+function hexWordToBin(hexWord){
+	}
+	hexadecimal = document.getElementById("hexadecimal");
+	binaire = document.getElementById("binaire");
+ 
+	decimal.value = parseInt(hexadecimal.value,16);
+	binaire.value = (parseInt(hexadecimal.value,16)).toString(2);
+	}
+
+	{
+	let binWord = "id_element";
+	id_element : update(binword)
+	return binWord;
+	}
+
+function hexDigitToBin(hexDigit){
+	upperCaseHexDigit = hexDigit.toUpperCase("hexadécimal")
+	upperCaseHexDigit = binDigit.toUpperCase("binaire")
+	return ;
+}
+
+function hexToBinMapping(hexadecimal){
+	upperCaseHexDigit = binDigit.toUpperCase("binaire")
+	const result() = {
+	"0":"0",
+	"1":"1",
+	"2":"10",
+	"3":"11",
+	"4":"100",
+	"5":"101",
+	"6":"110",
+	"7":"111",
+	"8":"1000",
+	"9":"1001",
+	"A":"1010",
+	"B":"1011",
+	"C":"1100",
+	"D":"1101",
+	"E":"1110",
+	"F":"1111"
+	}
+	return
+}
